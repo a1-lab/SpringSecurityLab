39266e8322095f55ae0a2db3c36111cfb8ae3a8d
a1-lab <alphamailix@gmail.com>
2022-12-16 23:37:07 +0200

    hello-app: initial commit

---
+++ hello-app/src/test/java/org/a1lab/hello/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.a1lab.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
